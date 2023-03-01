9f7508674930f134400b252287e1438cc2df060a
m1r4sh <120417301+m1r4sh@users.noreply.github.com>
2023-03-01 17:13:11 +0200

    Add files via upload

---
+++ src/Quotation.java
@@ -0,0 +1,7 @@
+public class Quotation {
+  String quote = "Remember no one can make you feel inferior without your consent.";
+  String author = "Eleanor Roosevelt";
+  public void display() {
+    System.out.println(quote+"\n - "+author);
+  }
+}
